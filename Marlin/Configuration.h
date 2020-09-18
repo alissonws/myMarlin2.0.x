2c94f317e0883d309945e273fef7601faf1c3cce
Alisson W. Corrêa <alissonwcorrea@gmail.com>
2020-09-18 08:32:35 -0300

    Marlin calibrado

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -747,14 +747,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 5120, 185.20 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2275.55, 98.773 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 200, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 3, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
